df2e2a7fa7324558a1497f4de1d0eaf00dc8d1c7
sak23042006 <138766433+sak23042006@users.noreply.github.com>
2023-09-22 18:38:41 +0530

    1 file modified

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 //button
 var strike = document.getElementById('strike');
 
-var reset = document.querySelector('reset');
+var reset = document.querySelector('#reset');
 
 //score
-var team1Score = document.querySelector('score-team1');
+var team1Score = document.querySelector('#score-team1');
 
-var team2Score = document.querySelector('score-team2');
+var team2Score = document.querySelector('#score-team2');
 
 //wicket
-var team1Wicket = document.querySelector('wicket-team1');
+var team1Wicket = document.querySelector('#wicket-team1');
 
-var team1Wicket = document.querySelector('wicket-team2');
+var team2Wicket = document.querySelector('#wicket-team2');
 
 //audio
 var strikeAudio = new Audio('http://bit.ly/so-ball-hit');
@@ -34,7 +34,7 @@ var balls2_initial = 0;
 
 var turn = 1;
 
-var possibleOutcomes = [0, 1, 2, 3, 4, 5, 6, 'W'];
+var possibleOutcomes = ['w', 1, 2, 3, 4, 5, 0, 6];
 
 strike.addEventListener('click', function () {
   strikeAudio.play();
@@ -42,4 +42,28 @@ strike.addEventListener('click', function () {
 
   var random =
     possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
+  console.log(random);
+  if (turn == 1) {
+    //india batting
+    balls1_initial++;
+    var ball = document.querySelector(
+      `#team1-supervisior div:nth-child(${balls1_initial})`
+    );
+    ball.innerHTML = random;
+
+    if (random == 'w') {
+      team1Wicket_initial++;
+    } else {
+      team1Score_initial += random;
+    }
+    if (balls1_initial == 6 || team1Wicket > 2) {
+      turn = 2;
+    }
+  }
+  console.log(team1Wicket_initial);
+  update1();
 });
+function update1() {
+  team1Score.innerHTML = team1Score_initial;
+  team1Wicket.innerHTML = team1Wicket_initial;
+}
