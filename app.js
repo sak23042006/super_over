2de9b285eb478ace97e15a71012b386ec591af4a
sak23042006 <138766433+sak23042006@users.noreply.github.com>
2023-10-02 19:24:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //button
-var strike = document.getElementById('strike');
+var strike = document.querySelector('#strike');
 
 var reset = document.querySelector('#reset');
 
@@ -36,12 +36,14 @@ var turn = 1;
 
 var possibleOutcomes = ['w', 1, 2, 3, 4, 5, 0, 6];
 
-strike.addEventListener('click', function () {
-  strikeAudio.play();
-  strikeAudio.currentTime = 0;
+strike.addEventListener('click', strikeButtonClicked);
 
-  var random =
-    possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
+function strikeButtonClicked(){
+  strikeAudio.pause()
+  strikeAudio.currentTime = 0;
+  strikeAudio.play()
+
+  var random = possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
   if (turn == 1) {
     //india batting
     balls1_initial++;
@@ -55,15 +57,61 @@ strike.addEventListener('click', function () {
     } else {
       team1Score_initial += random;
     }
-    if (balls1_initial == 6 || team1Wicket > 2) {
+    if (balls1_initial == 6 || team1Wicket_initial > 2) {
       turn = 2;
     }
-    update1();
+  update()
+}
+if (turn == 2) {
+  //india batting
+  balls2_initial++;
+  var ball = document.querySelector(
+    `#team2-supervisior div:nth-child(${balls2_initial})`
+  );
+  ball.innerHTML = random;
+
+  if (random == 'w') {
+    team2Wicket_initial++;
+  } else {
+    team2Score_initial += random;
   }
-});
-function update1() {
+  update()
+        if(team2Score_initial > team1Score_initial || team2Wicket == 2 || balls2_initial ==6 ){
+            turn = 3;
+            setTimeout(()=>{
+                gameOver();
+      },100)
+    }
+  }
+
+};
+function update() {
   team1Score.innerHTML = team1Score_initial;
   team1Wicket.innerHTML = team1Wicket_initial;
   team2Score.innerHTML = team2Score_initial;
   team2Wicket.innerHTML = team2Wicket_initial;
+}
+
+function gameOver(){
+  gameOverAudio.pause()
+  gameOverAudio.currentTime=0
+  gameOverAudio.play()
+  if(team1Score_initial>team2Score_initial){
+      alert("INDIA WIN")
+  }else if(team1Score_initial<team2Score_initial){
+      alert("PAK WINS")
+  }else if(team1Score_initial==team2Score_initial){
+      alert("It's is a tie!")
+  }
+  document.querySelectorAll(".ball").forEach(kalvium=>{
+      if(kalvium.innerHTML==""){
+          kalvium.innerHTML="X"
+          kalvium.style.backgroundColor = "grey"
+      }
+  }
+      )
+  }
+reset.addEventListener("click",resetFunction)
+function resetFunction(){
+  window.location.reload()
 }
